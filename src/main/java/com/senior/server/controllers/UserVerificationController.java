db7317cdeb70695c820c9f6b917bc2ffed306fe3
Akezhan Mussa <ake.mussa@gmail.com>
2020-10-13 00:41:01 +0600

    * created component for connecting with Mongo DB * created a basic logic id for Users * refactored code

---
+++ src/main/java/com/senior/server/controllers/UserVerificationController.java
@@ -0,0 +1,66 @@
+package com.senior.server.controllers;
+
+
+import com.senior.server.domain.User;
+import com.senior.server.repositories.UserRepository;
+import com.senior.server.services.PhoneNumberVerificationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestController
+@RequestMapping(path = "/api")
+public class UserVerificationController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserVerificationController.class);
+    private PhoneNumberVerificationService phoneNumberVerificationService;
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserVerificationController(PhoneNumberVerificationService phoneNumberVerificationService, UserRepository userRepository) {
+        this.phoneNumberVerificationService = phoneNumberVerificationService;
+        this.userRepository = userRepository;
+    }
+
+
+    @RequestMapping(path = "database-check", method = RequestMethod.POST)
+    public ResponseEntity<?> checkIdInDatabase(@RequestBody Map<String, String> requestBody){
+        String id = requestBody.getOrDefault("id", "-1");
+        User candidateUser = userRepository.getUserWithId(id);
+        if (candidateUser == null){
+            logger.info("User was not found");
+            Map<String, String> errorMessage = new HashMap();
+            errorMessage.put("errorMessage", "No such user with this id exists");
+            return new ResponseEntity<Map<String, String>>(errorMessage, HttpStatus.BAD_REQUEST);
+        }
+        logger.info("User was found");
+        return new ResponseEntity<User>(candidateUser, HttpStatus.OK);
+    }
+
+//    @RequestMapping(path = "start", method = RequestMethod.POST)
+//    public ResponseEntity<?> start(@RequestBody Map<String, String> requestBody) {
+//        String phoneNumber = requestBody.getOrDefault("phoneNumber", "+77015137789");
+//        String via = requestBody.getOrDefault("via", Verification.Channel.SMS.toString());
+//        Verification verification = phoneNumberVerificationService.start(phoneNumber, via);
+//        Map<String, String> rawRes = new HashMap();
+//        HttpStatus status = HttpStatus.BAD_REQUEST;
+//        if (verification.getValid()){
+//            rawRes.put("ans", "success");
+//            status = HttpStatus.OK;
+//        }else{
+//            rawRes.put("ans", "failure");
+//        }
+//        return new ResponseEntity<Map<String, String>>(rawRes, status);
+//    }
+
+}
---
+++ src/main/java/com/senior/server/repositories/UserRepositoryImpl.java
@@ -0,0 +1,28 @@
+package com.senior.server.repositories;
+
+import com.senior.server.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class UserRepositoryImpl implements UserRepository{
+
+    private MongoTemplate mongoTemplate;
+
+    @Autowired
+    public void setMongoTemplate(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    @Override
+    public User getUserWithId(String id) {
+        Query query = new Query(Criteria.where("id").is(id));
+        List<User> possibleCandidate = this.mongoTemplate.find(query, User.class);
+        return possibleCandidate.isEmpty() ? null : possibleCandidate.get(0);
+    }
+}
