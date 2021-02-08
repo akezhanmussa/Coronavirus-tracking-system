880e596e29e42b603ed084a7ea8d88d6a18f1407
ldgeanixr <ldgeanixr@gmail.com>
2021-02-08 22:53:53 +0600

    map bug fixed, circle added

--- CovidTracerApp/app/src/main/java/com/example/covidtracerapp/presentation/MapActivity.kt
+++ CovidTracerApp/app/src/main/java/com/example/covidtracerapp/presentation/MapActivity.kt
@@ -1,43 +1,109 @@
 package com.example.covidtracerapp.presentation
 
-import android.location.Address
-import android.location.Geocoder
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.location.Location
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.example.covidtracerapp.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.Circle
+import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
-import java.io.IOException
 
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private var locationPermissionGranted: Boolean = false
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private var gMap: GoogleMap? = null
+
+    private val FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION
+    private val COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1
+    private val ZOOM_DEFAULT = 5f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap?) {
+        gMap = googleMap
+
+        val permissions = arrayOf<String>(FINE_LOCATION, COARSE_LOCATION)
+
+        if (locationPermissionGranted){
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    permissions,
+                    LOCATION_PERMISSION_REQUEST_CODE
+                )
+                return
+            }
+
+        }
+
+        val location = fusedLocationProviderClient.lastLocation
+        location.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val currentLocation = task.result as Location
+                val latLng = LatLng(currentLocation.latitude, currentLocation.longitude)
+                gMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 25f))
+                gMap?.isMyLocationEnabled = true
+
+                val circle: Circle = gMap!!.addCircle(
+                    CircleOptions()
+                        .center(latLng)
+                        .radius(1000.0)
+                        .strokeColor(Color.RED)
+                        .fillColor(Color.RED)
+                )
+            }
+        }
+
 
     }
 
 
-    private fun getLatLngOfCity(query: String?): LatLng? {
-        val geocoder = Geocoder(this)
-        var list: List<Address> = ArrayList()
-        try {
-            list = geocoder.getFromLocationName(query, 1)
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-        if (list.size != 0) {
-            val address: Address = list[0]
-            val lat: Double = address.latitude
-            val lon: Double = address.longitude
-            return LatLng(lat, lon)
-        }
-        return null
+
+
+
+
+
+
+
+    companion object {
+        private const val TAG = "MapActivity"
+        private const val DEFAULT_ZOOM = 15
+        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
+
+        // Keys for storing activity state.
+        private const val KEY_CAMERA_POSITION = "camera_position"
+        private const val KEY_LOCATION = "location"
+
+        // Used for selecting the current place.
+        private const val M_MAX_ENTRIES = 5
     }
 
 
